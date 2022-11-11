ea12da6692fab08bd956a3dc4e21d739f130ac63
LarysaHliieva <lora.glievaya@gmail.com>
2022-11-11 15:45:22 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+    form: document.querySelector(".login-form"),
+}
 
+const onSubmit = (event) => {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (!email.value || !password.value.trim()) {
+        alert("Всі поля повинні бути заповнені");
+        return;
+    }
+
+    // const formData = new FormData(event.currentTarget);
+
+    const formData = {
+        email: `${email.value}`,
+        password: `${password.value}`,
+    }
+    
+    console.log(formData);
+    event.currentTarget.reset();
+}
+
+refs.form.addEventListener("submit", onSubmit);
