b65d8f2b77982ff65c2925d8a3f16344ff3a4ec3
LarysaHliieva <lora.glievaya@gmail.com>
2022-11-11 12:23:15 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIgredientsEl = document.querySelector("#ingredients");
+
+const createLiIgredients = (item => {
+  const liIgredientsEl = document.createElement("li");
+  liIgredientsEl.textContent = item;
+  liIgredientsEl.classList.add("item");
+  return liIgredientsEl;
+})
+
+const listIgredients = ingredients.map(createLiIgredients);
+
+listIgredientsEl.append(...listIgredients);
+
+
+
