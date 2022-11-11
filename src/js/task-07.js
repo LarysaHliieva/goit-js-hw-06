bc5959cdb05bda25c72197a3fca3373b13b9c890
LarysaHliieva <lora.glievaya@gmail.com>
2022-11-11 15:16:44 +0200

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,13 @@
+const refs = {
+    input: document.querySelector("#font-size-control"),
+    text: document.querySelector("#text"),
+}
+
+const onChangeFontSize = (event) => {
+    const value = event.currentTarget.value;
+
+    refs.text.style.fontSize = `${value}px`;
+}
+
+refs.input.addEventListener("input", onChangeFontSize);
+
